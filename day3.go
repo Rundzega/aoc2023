feade94bad588a76168bffda2cf8498105551500
rafael <Rundzega>
2023-12-10 21:46:39 -0300

    day 3 part 2

--- day3.go
+++ day3.go
@@ -56,8 +56,48 @@ func fixEngine(fileInput string) int {
 	return result
 }
 
+func fixGear(fileInput string) int {
+	file, err := os.Open(fileInput)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+    result := 0
+    
+    // Initialize all lines 
+    scanner.Scan()
+    topLine := ""
+    currentLine := ""
+    bottomLine := scanner.Text()
+	for scanner.Scan() {
+        topLine = currentLine
+        currentLine = bottomLine
+        bottomLine = scanner.Text()
+        result += computeGearLine(currentLine, topLine, bottomLine)
+	}
+    // compute Last line
+    result += computeGearLine(bottomLine, currentLine, "")
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return result
+
+}
+
 func findNumbers(line string) [][]int {
     return findIndex(line, `\d+`)
+}
+
+func findStars(line string) []int {
+    starsIdxPair := findIndex(line, `\*`)
+    starsIdx := make([]int, 0)
+    
+    for _, idx := range starsIdxPair {
+        starsIdx = append(starsIdx, idx[0])
+    }
+    return starsIdx
 }
 
 func findIndex(line, regex string) [][]int {
@@ -65,7 +105,6 @@ func findIndex(line, regex string) [][]int {
 }
 
 func matchAdjacent(unmatchedNumsIdx [][]int, line string) (matched, unmatched [][]int) {
-    //fmt.Printf("unmatched: %v, \n", unmatchedNumsIdx)
     matched = make([][]int, 0)
     unmatched = make([][]int, 0)
     for _, indexes := range unmatchedNumsIdx {
@@ -81,11 +120,8 @@ func matchAdjacent(unmatchedNumsIdx [][]int, line string) (matched, unmatched []
         }
 
         if substring := line[leftLimit : rightLImit]; isMatch(`[^\d\.]`, substring){
-           // fmt.Printf("found match\n")
             matched = append(matched, indexes)
-            //fmt.Printf("%v\n", matched)
         } else {
-            //fmt.Printf("not found\n")
             unmatched = append(unmatched, indexes)
         }
     }
@@ -96,6 +132,67 @@ func isMatch(regex, line string) bool {
     return regexp.MustCompile(regex).MatchString(line)
 }
 
+func computeGearLine(gearLine, aboveLine, belowLine string) int {
+    lineTotal := 0
+    for _, starIdx := range findStars(gearLine) {
+        qtdAdj := 0
+        gearRatio := 1
+        qtdAdjLine := 0
+        lineGearRatio := 1
+        if (aboveLine != "") {
+            aboveNums := findNumbers(aboveLine)
+            qtdAdjLine, lineGearRatio = checkAdjacentToStar(starIdx, aboveNums, aboveLine)
+            qtdAdj += qtdAdjLine
+            gearRatio *= lineGearRatio
+        }
+        if (belowLine != "") {
+            belowNums := findNumbers(belowLine)
+            qtdAdjLine, lineGearRatio = checkAdjacentToStar(starIdx, belowNums, belowLine)
+            qtdAdj += qtdAdjLine
+            gearRatio *= lineGearRatio
+        }
+        gearLineNums := findNumbers(gearLine)
+        qtdAdjLine, lineGearRatio = checkAdjacentToStar(starIdx, gearLineNums, gearLine)
+        qtdAdj += qtdAdjLine
+        gearRatio *= lineGearRatio
+        if qtdAdj == 2 {
+            lineTotal += gearRatio
+        }
+    }
+    return lineTotal
+}
+
+func checkAdjacentToStar(starIdx int, numsPos [][]int, line string) (qtdAjd, gearRatio int) {
+    qtdAjd = 0
+    gearRatio = 1
+    matchedNums := make([][]int, 0)
+    for _, numPos := range numsPos {
+        
+        numStart := numPos[0]
+        numEnd := numPos[1] - 1
+        starIdxStart := starIdx - 1
+        if starIdxStart < 0 {
+            starIdxStart = 0
+        }
+        starIdxEnd := starIdx + 1
+        if starIdxEnd == len(line) {
+            starIdxEnd = starIdx
+        }
+        if (numStart >= starIdxStart && numStart <= starIdxEnd) ||
+            (numEnd >= starIdxStart && numEnd <= starIdxEnd) ||
+            (starIdxStart <= numEnd && starIdxStart >= numStart) ||
+            (starIdxEnd <= numEnd && starIdxEnd >= numStart) {
+                qtdAjd++
+                matchedNums = append(matchedNums, numPos)
+            }
+    }
+    if qtdAjd > 0 {
+        gearRatio = multiplyMatchedNumbers(matchedNums, line)
+    } 
+    return
+}
+
+
 func sumMatchedNumbers(numbersIdx [][]int, line string) int {
     total := 0
     for _, idx := range numbersIdx {
@@ -103,8 +200,20 @@ func sumMatchedNumbers(numbersIdx [][]int, line string) int {
         if err != nil {
             panic(err)
         }
-       // fmt.Printf("%v\n", number)
         total += number
     }
     return total
 }
+
+func multiplyMatchedNumbers(numbersIdx [][]int, line string) int {
+    total := 1
+    for _, idx := range numbersIdx {
+        number, err := strconv.Atoi(line[idx[0]:idx[1]])
+        if err != nil {
+            panic(err)
+        }
+        total *= number
+    }
+    return total
+}
+
--- day3_test.go
+++ day3_test.go
@@ -11,3 +11,11 @@ func TestFixEngine(t *testing.T) {
         t.Fatalf("Wanted %v and got %v", want, actual)
     }
 }
+
+func TestFixGear(t *testing.T) {
+    want := 467835
+    actual := fixGear("data/day3_test.txt")
+    if want != actual {
+        t.Fatalf("Wanted %v and got %v", want, actual)
+    }
+}
